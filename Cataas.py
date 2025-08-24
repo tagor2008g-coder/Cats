63f46873ba8692a1aaa7d177fb1f7f12175f2b27
tagor2008g-coder <tagor2008g@g,com>
2025-08-24 22:51:06 +0300

    добавили выпадающий список тегов

--- Cataas.py
+++ Cataas.py
@@ -1,8 +1,13 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+# Список доступных тегов
+ALLOWED_TAGS = [
+    'sleep', 'jump', 'smile', 'fight', 'black', 'white', 'red', 'siamese', 'bengal'
+]
 
 def load_image(url):
     try:
@@ -12,16 +17,14 @@ def load_image(url):
         img = Image.open(image_data)
         img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
-    except tException as e:
+    except Exception as e:
         print(f"Ошибка при загрузке изображения: {e}")
         return None
 
-
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_with_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
     img = load_image(url_with_tag)
-
     if img:
         new_window = Toplevel()
         new_window.title("Cat Image")
@@ -30,13 +33,32 @@ def open_new_window():
         label.image = img
         label.pack()
 
+window = Tk()
+window.title("Cats!")
+window.geometry("600x520")
 
-def exit_app():
-    window.destroy()
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=window.destroy)
+
+# Метка "Выбери тег"
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=ALLOWED_TAGS)
+tag_combobox.pack()
+
+window.mainloop()
+
 
 
 window = Tk()
-…
+
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit_app)
 
